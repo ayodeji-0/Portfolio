19e933a63cd8d56688fb9f3f456828c9b7e51fe6
Ayodeji Adeniyi <67892647+ayodeji-0@users.noreply.github.com>
2024-06-10 23:12:44 +0100

    Biomimetic Arm Init

---
+++ Others/BioMimetic Hand/Readme.md
@@ -0,0 +1,3 @@
+## Biomimetic Hand
+Aim of Personal Project is to improve on designs I have found online, initiating my own project focusing on replication of the thumb's motion. The thumb has 6 ish DOF that currently prove hard to replicate in the prosthetics landscape
+ 
---
+++ Others/BioMimetic Hand/fingerPos.ino
@@ -0,0 +1,38 @@
+// Ayodeji Adeniyi 12/2023
+// BioMimetic Hand
+// Code for reading finger positions from control glove
+// Run on an ESP32, reads pot values and sends to master/ sets finger positions, depending on mode
+// Wireless communication using ESP-NOW protocol
+
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+#include <Servp.h>
+#include <ESP_Now.h>
+
+#define MASTER_MAC {0x24, 0x0A, 0xC4, 0x0A, 0x0B, 0x80}
+#define SLAVE_MAC {0x24, 0x0A, 0xC4, 0x0A, 0x0B, 0x80}
+
+int fingerPos[5] = {0, 0, 0, 0, 0};
+
+void receiveData(int byteCount) {
+  while (Wire.available()) {
+    for (int i = 0; i < 5; i++) {
+      fingerPos[i] = Wire.read();
+    }
+  }
+}
+
+void readFingerPos() {
+  for (int i = 0; i < 5; i++) {
+    Serial.print(fingerPos[i]);
+    Serial.print(" ");
+  }
+  Serial.println();
+}
+
+void setup() {
+  Wire.begin(SLAVE_ADDRESS);
+  Wire.onReceive(receiveData);
+  Serial.begin(9600);
+}
+
